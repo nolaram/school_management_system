25815b5865590bb36846772549136fdf304be521
nolaram <marloncopino10@gmail.com>
2026-07-06 10:23:16 +0800

    adminseeder_added

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Authenticatable
 {
@@ -45,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
     }
 }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Role::create([
+            'name' => 'Admin'
+        ]);
+
+        Role::create([
+            'name' => 'Teacher'
+        ]);
+
+        Role::create([
+            'name' => 'Student'
+        ]);
+    }
+}
